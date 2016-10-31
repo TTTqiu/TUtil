65a0ddf68e45476a758275077e4fa3eaa7882e62
TTTqiu <qiutiangorkmork@163.com>
2016-10-31 20:19:25 +0800

    first commit

---
+++ library/src/main/java/com/tttqiu/library/Util.java
@@ -0,0 +1,13 @@
+package com.tttqiu.library;
+
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2016/10/31.
+ */
+
+public class Util {
+    public static void q(){
+        Log.d("ppqq","1");
+    }
+}
